7f915f9b4f69b1a71c43323500b33abf65593228
mickdiggin <m_degan@mail.fhsu.edu>
2024-02-15 16:23:33 -0600

    Initial Commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+df = pd.DataFrame(
+    {
+        "Name": [
+            "Braund, Mr. Owen Harris",
+            "Allen, Mr. William Henry",
+            "Bonnell, Miss. Elizabeth",
+        ],
+        "Age": [22, 35, 58],
+        "Sex": ["male", "male", "female"],
+    }
+)
+
+level = pd.Series(['sophmore', 'freshman', 'senior'], name='Student Level')
+
+print(level)
+
+#print(list(df.columns))
+
+print(df['Age'])
+
+ages = pd.Series([22, 35, 58], name="Age")
